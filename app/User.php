c680d8ace7b98462051894b4e7cee06f106fb1d3
Alejandro González Reyes <alexglzrys@gmail.com>
2020-01-15 18:35:03 -0600

    Create routes and assign permissions

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Caffeinated\Shinobi\Concerns\HasRolesAndPermissions;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -9,6 +10,8 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use Notifiable;
+    // Agregar el trait necesario para asignar con roles y permisos a los usuarios 
+    use HasRolesAndPermissions;
 
     /**
      * The attributes that are mass assignable.
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,35 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Para poder trabajar con roles y permisos, previamente tiene que haber un sistema de login
+Route::middleware(['auth'])->group(function() {
+
+    // Establecer los permisos a las rutas del modelo Product
+    Route::get('/products', 'ProductController@index')->name('products.index')->middleware('can:products.index');
+    Route::get('/products/create', 'ProductController@create')->name('products.create')->middleware('can:products.store');
+    Route::post('/products', 'ProductController@store')->name('products.store')->middleware('can:products.store');
+    Route::get('/products/{product}', 'ProductController@show')->name('products.show')->middleware('can:products.show');
+    Route::get('/products/{product}/edit', 'ProductController@edit')->name('products.edit')->middleware('can:products.edit');
+    Route::put('/products/{product}', 'ProductController@update')->name('products.update')->middleware('can:products.edit');
+    Route::delete('/products/{product}', 'ProductController@destroy')->name('products.destroy')->middleware('can:products.destroy');
+
+    // Establecer los permisos a las rutas del modelo Role
+    Route::get('/roles', 'RoleController@index')->name('roles.index')->middleware('can:roles.index');
+    Route::get('/roles/create', 'RoleController@create')->name('roles.create')->middleware('can:roles.store');
+    Route::post('/roles', 'RoleController@store')->name('roles.store')->middleware('can:roles.store');
+    Route::get('/roles/{product}', 'RoleController@show')->name('roles.show')->middleware('can:roles.show');
+    Route::get('/roles/{product}/edit', 'RoleController@edit')->name('roles.edit')->middleware('can:roles.edit');
+    Route::put('/roles/{product}', 'RoleController@update')->name('roles.update')->middleware('can:roles.edit');
+    Route::delete('/roles/{product}', 'RoleController@destroy')->name('roles.destroy')->middleware('can:roles.destroy');
+
+    // Establecer los permisos a las rutas del modelo User
+    // Todo usuario tiene derecho a registrarse, mas adelante se le asignan sus permisos correspondientes
+    // El registro se lleva a cabo desde el formulario de auth
+    Route::get('/users', 'UserController@index')->name('users.index')->middleware('can:users.index');
+    Route::get('/users/{product}', 'UserController@show')->name('users.show')->middleware('can:users.show');
+    Route::get('/users/{product}/edit', 'UserController@edit')->name('users.edit')->middleware('can:users.edit');
+    Route::put('/users/{product}', 'UserController@update')->name('users.update')->middleware('can:users.edit');
+    Route::delete('/users/{product}', 'UserController@destroy')->name('users.destroy')->middleware('can:users.destroy');
+
+});
